b4e9a9853d64671ec19ac5b10c72084c58562c3b
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-24 21:41:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,18 +9,13 @@ class Circle {
     return this.radius * 2;
   }
   
-  set diameter(diameter) {
-    this.radius = diameter / 2
-  }
+  // set diameter(diameter) {
+  //   this.radius = diameter / 2
+  // }
   
   get circumference(){
     return this.radius * 2 * Math.PI
   }
-  
-  set circumstance(diameter) {
-    this.radius = diameter / 2
-  }
-
   
   get area(){
     return this.radius * this.radius * Math.PI
